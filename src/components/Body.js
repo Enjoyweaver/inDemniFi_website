52ec979f65d5d6d380448988d6f395dbaa1e35fb
Michael <enjoyweaver@gmail.com>
2024-12-23 08:22:03 -0500

    wallet intro

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,15 +12,15 @@ const Body = () => {
         width: "60vw",
       }}
     >
-      We're currently developing the Wallet Insurance Model (WIM SDK) to enable
-      crypto wallets to provide insurance and risk management services to their
-      users.
+      The Insurance Wallet is here to help you understand and manage risks as
+      you navigate Web3. It gives you insight into your onchain activities,
+      showing you where risks might be and helping you stay in control.
       <br />
       <br />
-      During the development of the WIM SDK, we've come to realize that our
-      wallet risk analysis tool could be a valuable tool for others. With the
-      support of a grant from Covalent for API credits over the next year, we
-      are excited to offer this tool to the public free of charge.
+      During the development of the Insurance Wallet, we've come to realize that
+      our wallet risk analysis tool could be a valuable tool for others. With
+      the support of a grant from Covalent for API credits over the next year,
+      we are excited to offer this tool to the public free of charge.
       <br />
       <br />
       Connect your wallet to view analysis of your transaction history and open
